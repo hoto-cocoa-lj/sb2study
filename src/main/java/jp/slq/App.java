85b7fb56239c48f5cbcac30b7842c257355416f1
Ironi333 <657578087@qq.com>
2021-02-03 14:54:25 +0800

    update笔记

--- src/main/java/jp/slq/App.java
+++ src/main/java/jp/slq/App.java
@@ -14,17 +14,19 @@ public class App {
         ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
         User user = run.getBean(User.class);
         Dog dog = run.getBean(Dog.class);
-        //Cat cat = run.getBean(Cat.class);
+        //Cat cat = run.getBean(Cat.class);                            //报错有两个猫
         String[] users = run.getBeanNamesForType(User.class);
-        System.out.println(run.containsBean("component_dog"));
+        System.out.println(run.containsBean("component_dog"));      //true
         String[] dogs = run.getBeanNamesForType(Dog.class);
         String[] cats = run.getBeanNamesForType(Cat.class);
-        System.out.println(user);
-        System.out.println(dog);
+        System.out.println(user);       //User(name=user1, age=11, dog=Dog(nickname=reddog))
+        System.out.println(dog);        //Dog(nickname=reddog)
         //System.out.println(cat);
-        System.out.println(Arrays.toString(users));
-        System.out.println(Arrays.toString(dogs));
-        System.out.println(Arrays.toString(cats));
+        System.out.println(Arrays.toString(users));     //[user1]
+        System.out.println(Arrays.toString(dogs));      //[component_dog]
+        System.out.println(Arrays.toString(cats));      //[jp.slq.pojo.Cat, cat-jp.slq.pojo.Cat]
+
+
 
     }
 }
